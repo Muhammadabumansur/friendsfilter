03263de37c13246155ba503e6b97c47b329a16a0
Magomed Kandaev <muhammad-114@mail.ru>
2016-03-30 21:27:36 +0300

    refactoring

--- app/js/main.js
+++ app/js/main.js
@@ -37,124 +37,133 @@ new Promise(function(resolve) {
                 if (response.error) {
                     reject(new Error(response.error.error_msg));
                 } else {
-                    var friendsArray = [];
+                    var allFriendsArray = [];
                     var filteredFriendsArray = [];
-                      if (localStorage.friendsArray) {
-                        friendsArray = JSON.parse(localStorage.friendsArray);
+
+                    function renderFriends(friendStatus, item) {
+                        var li = document.createElement("li");
+                        li.id = "id" + item.uid;
+                        li.setAttribute('draggable', true);
+                        var friendPhoto = document.createElement("img");
+                        friendPhoto.className = 'friend_photo';
+                        friendPhoto.setAttribute('src', item.photo_50);
+                        friendPhoto.setAttribute('draggable', false);
+                        li.appendChild(friendPhoto);
+                        var friendName = document.createElement("div");
+                        friendName.className = 'friend_name';
+                        friendName.textContent = item.first_name + " " + item.last_name;
+                        li.appendChild(friendName);
+                        var friendMove = document.createElement("div");
+                        if (friendStatus === "all") {
+                                 li.className = 'all_friends_list_item';
+                                 friendMove.className = 'friend_add';
+                                 li.appendChild(friendMove);
+                                allFriendsList.appendChild(li); 
+                       } 
+                       if (friendStatus === "filtered") {
+                                li.className = 'filtered_friends_list_item';
+                                friendMove.className = 'friend_remove';
+                                li.appendChild(friendMove);
+                                 filteredFriendsList.appendChild(li); 
+                       }
+                               
+                    }
+
+                    function moveFriend(friendsList, friendsArray, otherArray, li) {
+                            friendsList.insertBefore(li, friendsList.children[0]);
+                            li.className = "all_friends_list_item";
+                            var friendMove = document.createElement('div');
+                            if (friendsList === allFriendsList) {
+                                    li.className = "all_friends_list_item";
+                                    li.removeChild(li.getElementsByClassName('friend_remove')[0]);;
+                                    friendMove.className = 'friend_add';
+                            }
+                            if (friendsList === filteredFriendsList) {
+                                    li.className = "filtered_friends_list_item";
+                                    li.removeChild(li.getElementsByClassName('friend_add')[0]);;
+                                    friendMove.className = 'friend_remove';
+                            }
+                            li.appendChild(friendMove);
+                            friendsArray.forEach(function(item, i) {
+                                    if ('id' + item.uid == li.id) {
+                                            otherArray.splice(0, 0, item);
+                                            friendsArray.splice(i, 1);
+                                    }
+                            })
+                    }
+
+                    if (localStorage.allFriendsArray) {
+                        allFriendsArray = JSON.parse(localStorage.allFriendsArray);
                         } else {
-                            friendsArray = response.response;
+                            allFriendsArray = response.response;
                         }
                     if (localStorage.filteredFriendsArray) {
                         filteredFriendsArray = JSON.parse(localStorage.filteredFriendsArray);
                     }  else {
                         filteredFriendsArray = [];
                     }
-                    friendsArray.forEach(function(item, i) {
-                        var li = document.createElement("li");
-                        li.className = 'all_friends_list_item';
-                        li.id = "id" + item.uid;
-                        li.setAttribute('draggable', true);
-                        var friendPhoto = document.createElement("img");
-                        friendPhoto.className = 'friend_photo';
-                        friendPhoto.setAttribute('src', item.photo_50);
-                        friendPhoto.setAttribute('draggable', false);
-                        li.appendChild(friendPhoto);
-                        var friendName = document.createElement("div");
-                        friendName.className = 'friend_name';
-                        friendName.textContent = item.first_name + " " + item.last_name;
-                        li.appendChild(friendName);
-                        var friendAdd = document.createElement("div");
-                        friendAdd.className = 'friend_add';
-                        li.appendChild(friendAdd);
-                        allFriendsList.appendChild(li);
+                    allFriendsArray.forEach(function(item) {
+                       renderFriends('all', item);
                     });
 
-                     filteredFriendsArray.forEach(function(item, i) {
-                        var li = document.createElement("li");
-                        li.className = 'filtered_friends_list_item';
-                        li.id = "id" + item.uid;
-                        li.setAttribute('draggable', true);
-                        var friendPhoto = document.createElement("img");
-                        friendPhoto.className = 'friend_photo';
-                        friendPhoto.setAttribute('src', item.photo_50);
-                        friendPhoto.setAttribute('draggable', false);
-                        li.appendChild(friendPhoto);
-                        var friendName = document.createElement("div");
-                        friendName.className = 'friend_name';
-                        friendName.textContent = item.first_name + " " + item.last_name;
-                        li.appendChild(friendName);
-                        var friendRemove = document.createElement("div");
-                        friendRemove.className = 'friend_remove';
-                        li.appendChild(friendRemove);
-                        filteredFriendsList.appendChild(li);
+                     filteredFriendsArray.forEach(function(item) {
+                        renderFriends('filtered', item);
                     });
 
                      friends.addEventListener('click', function (e) {
                         e.preventDefault();
                         if (e.target.className == "friend_add") {
                             var li =  e.target.closest('li');
-                            filteredFriendsList.insertBefore(li, filteredFriendsList.children[0]);
-                            li.className = "filtered_friends_list_item";
-                            li.removeChild(e.target);
-                            var friendRemove = document.createElement('div');
-                            friendRemove.className = 'friend_remove';
-                             li.appendChild(friendRemove);
-                            friendsArray.forEach( function(item, i) {
-                                if ('id' + item.uid == li.id) {
-                                    filteredFriendsArray.splice(0, 0, item);
-                                    friendsArray.splice(i, 1);
-                                }
-                            })
+
+                            moveFriend(filteredFriendsList, allFriendsArray, filteredFriendsArray, li);
+                            // filteredFriendsList.insertBefore(li, filteredFriendsList.children[0]);
+                            // li.className = "filtered_friends_list_item";
+                            // li.removeChild(e.target);
+                            // var friendRemove = document.createElement('div');
+                            // friendRemove.className = 'friend_remove';
+                            //  li.appendChild(friendRemove);
+                            // allFriendsArray.forEach( function(item, i) {
+                            //     if ('id' + item.uid == li.id) {
+                            //         filteredFriendsArray.splice(0, 0, item);
+                            //         allFriendsArray.splice(i, 1);
+                            //     }
+                            // })
 
                         }
 
                         if (e.target.className == "friend_remove") {
                             var li =  e.target.closest('li');
-                            allFriendsList.insertBefore(li, allFriendsList.children[0]);
-                            li.className = "all_friends_list_item";
-                            li.removeChild(e.target);
-                            var friendAdd = document.createElement('div');
-                            friendAdd.className = 'friend_add';
-                            li.appendChild(friendAdd);
-                            filteredFriendsArray.forEach( function(item, i) {
-                                if ('id' + item.uid == li.id) {
-                                    friendsArray.splice(0, 0, item);
-                                    filteredFriendsArray.splice(i, 1);
-                                }
-                            })
+
+                            moveFriend(allFriendsList, filteredFriendsArray, allFriendsArray, li);
+                            // allFriendsList.insertBefore(li, allFriendsList.children[0]);
+                            // li.className = "all_friends_list_item";
+                            // li.removeChild(e.target);
+                            // var friendAdd = document.createElement('div');
+                            // friendAdd.className = 'friend_add';
+                            // li.appendChild(friendAdd);
+                            // filteredFriendsArray.forEach( function(item, i) {
+                            //     if ('id' + item.uid == li.id) {
+                            //         allFriendsArray.splice(0, 0, item);
+                            //         filteredFriendsArray.splice(i, 1);
+                            //     }
+                            // })
                         }
                     });
 
                     search.addEventListener('input', function (e) {
                          if (e.target.className == "search_friends") {
                             var allResArray = [];
-                            allResArray = friendsArray.filter(function (item) {
+                            allResArray = allFriendsArray.filter(function (item) {
                                 if (e.target.value) {
                                     return (item.first_name.toLowerCase().indexOf(e.target.value.trim().toLowerCase()) !== -1) || (item.last_name.toLowerCase().indexOf(e.target.value.trim().toLowerCase()) !== -1);
                                 } else {
-                                    return friendsArray;
+                                    return allFriendsArray;
                                 }                 
                             });
 
                             allFriendsList.innerHTML = "";
-                            allResArray.forEach( function(item, i) {
-                                var li = document.createElement("li");
-                                li.className = 'all_friends_list_item';
-                                li.id = "id" + item.uid;
-                                li.setAttribute('draggable', true);
-                                var friendPhoto = document.createElement("img");
-                                friendPhoto.className = 'friend_photo';
-                                friendPhoto.setAttribute('src', item.photo_50);
-                                friendPhoto.setAttribute('draggable', false);
-                                li.appendChild(friendPhoto);
-                                var friendName = document.createElement("div");
-                                friendName.className = 'friend_name';
-                                friendName.textContent = item.first_name + " " + item.last_name;
-                                li.appendChild(friendName);
-                                var friendAdd = document.createElement("div");
-                                friendAdd.className = 'friend_add';
-                                li.appendChild(friendAdd);
-                                allFriendsList.appendChild(li);
+                            allResArray.forEach( function(item) {
+                               renderFriends('all', item);
                             });
                          }
 
@@ -169,24 +178,8 @@ new Promise(function(resolve) {
                             });
 
                             filteredFriendsList.innerHTML = "";
-                            filteredResArray.forEach( function(item, i) {
-                                var li = document.createElement("li");
-                                li.className = 'filtered_friends_list_item';
-                                li.id = "id" + item.uid;
-                                li.setAttribute('draggable', true);
-                                var friendPhoto = document.createElement("img");
-                                friendPhoto.className = 'friend_photo';
-                                friendPhoto.setAttribute('src', item.photo_50);
-                                friendPhoto.setAttribute('draggable', false);
-                                li.appendChild(friendPhoto);
-                                var friendName = document.createElement("div");
-                                friendName.className = 'friend_name';
-                                friendName.textContent = item.first_name + " " + item.last_name;
-                                li.appendChild(friendName);
-                                var friendRemove = document.createElement("div");
-                                friendRemove.className = 'friend_remove';
-                                li.appendChild(friendRemove);
-                                filteredFriendsList.appendChild(li);
+                            filteredResArray.forEach( function(item) {
+                                renderFriends('filtered', item);
                             });
                          }
 
@@ -203,18 +196,20 @@ new Promise(function(resolve) {
                         e.preventDefault();
                         var data = e.dataTransfer.getData("text");
                         var li = document.getElementById(data);
-                        filteredFriendsList.insertBefore(li, filteredFriendsList.children[0]);
-                        li.className = "filtered_friends_list_item";
-                        li.removeChild(li.getElementsByClassName('friend_add')[0]);
-                        var friendRemove = document.createElement('div');
-                        friendRemove.className = 'friend_remove';
-                        li.appendChild(friendRemove);
-                        friendsArray.forEach( function(item, i) {
-                            if ('id' + item.uid == data) {
-                                filteredFriendsArray.splice(0, 0, item);
-                                friendsArray.splice(i, 1);
-                            }
-                        })                        
+
+                        moveFriend(filteredFriendsList, allFriendsArray, filteredFriendsArray, li);
+                        // filteredFriendsList.insertBefore(li, filteredFriendsList.children[0]);
+                        // li.className = "filtered_friends_list_item";
+                        // li.removeChild(li.getElementsByClassName('friend_add')[0]);
+                        // var friendRemove = document.createElement('div');
+                        // friendRemove.className = 'friend_remove';
+                        // li.appendChild(friendRemove);
+                        // allFriendsArray.forEach( function(item, i) {
+                        //     if ('id' + item.uid == data) {
+                        //         filteredFriendsArray.splice(0, 0, item);
+                        //         allFriendsArray.splice(i, 1);
+                        //     }
+                        // })                        
                      })
 
                      filteredFriendsList.addEventListener('dragover', function (e) {
@@ -234,18 +229,20 @@ new Promise(function(resolve) {
                         e.preventDefault();
                         var data = e.dataTransfer.getData("text");
                         var li = document.getElementById(data);
-                        allFriendsList.insertBefore(li, allFriendsList.children[0]);
-                        li.className = "all_friends_list_item";
-                        li.removeChild(li.getElementsByClassName('friend_remove')[0]);
-                        var friendAdd = document.createElement('div');
-                        friendAdd.className = 'friend_add';
-                        li.appendChild(friendAdd);
-                        filteredFriendsArray.forEach( function(item, i) {
-                            if ('id' + item.uid == data) {
-                                friendsArray.splice(0, 0, item);
-                                filteredFriendsArray.splice(i, 1);
-                            }
-                        })                        
+
+                        moveFriend(allFriendsList, filteredFriendsArray, allFriendsArray, li);
+                        // allFriendsList.insertBefore(li, allFriendsList.children[0]);
+                        // li.className = "all_friends_list_item";
+                        // li.removeChild(li.getElementsByClassName('friend_remove')[0]);
+                        // var friendAdd = document.createElement('div');
+                        // friendAdd.className = 'friend_add';
+                        // li.appendChild(friendAdd);
+                        // filteredFriendsArray.forEach( function(item, i) {
+                        //     if ('id' + item.uid == data) {
+                        //         allFriendsArray.splice(0, 0, item);
+                        //         filteredFriendsArray.splice(i, 1);
+                        //     }
+                        // })                        
                      })
 
                      allFriendsList.addEventListener('dragover', function (e) {
@@ -254,10 +251,8 @@ new Promise(function(resolve) {
                      }) 
 
                      saveFriends.addEventListener('click', function () {
-                        localStorage.setItem('friendsArray', JSON.stringify(friendsArray));
+                        localStorage.setItem('allFriendsArray', JSON.stringify(allFriendsArray));
                         localStorage.setItem('filteredFriendsArray', JSON.stringify(filteredFriendsArray));
-                          // localStorage.friendsArray = friendsArray;
-                          // localStorage.filteredFriendsArray = filteredFriendsArray;                           
                      })
 
                     resolve();
